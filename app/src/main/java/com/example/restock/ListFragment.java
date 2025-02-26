45a3f7da45557e3af8e522895469758f5703a2ce
Angelina Garcia <ASGarcia2@student.fullsail.edu>
2025-02-25 22:11:35 -0600

    ListFragment is now displayed correctly in the emulator
    Files I changed:
    ListFragment and fragment_list
    Details of changes:
    Updated onCreateView to reference the correct layout

--- app/src/main/java/com/example/restock/ListFragment.java
+++ app/src/main/java/com/example/restock/ListFragment.java
@@ -54,19 +54,20 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_list_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        // Find the RecyclerView inside the layout
+        RecyclerView recyclerView = view.findViewById(R.id.listRecyclerView);
 
         // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS));
+        Context context = view.getContext();
+        if (mColumnCount <= 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
         }
+        recyclerView.setAdapter(new MyItemRecyclerViewAdapter(PlaceholderContent.ITEMS));
+
         return view;
     }
 }
